1a0bea30d12ae1a84be32f3c2376506ed1c2cfac
mosalehkamali <109099402+mosalehkamali@users.noreply.github.com>
2024-03-22 17:02:03 +0430

    first commit

---
+++ pages/homes/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeDetails() {
+  return (
+    <h1>
+      home details
+    </h1>
+  )
+}
+
+export default HomeDetails
--- pages/homes/index.js
+++ pages/homes/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function index() {
   return (
     <h1>
-      home page
+      all homes page
     </h1>
   )
 }
